855e77b1fbee2f6d8b5c689e0f33edfc510b2936
horitada <m.horiguchi.info@gmail.com>
2018-12-28 17:53:38 +0900

    コンフリクト解消ミスを修正

--- Model/Nc2ToNc3PhotoAlbum.php
+++ Model/Nc2ToNc3PhotoAlbum.php
@@ -113,6 +113,7 @@ class Nc2ToNc3PhotoAlbum extends Nc2ToNc3AppModel {
 		$PhotoAlbumsComponent = new PhotoAlbumsComponent(new ComponentCollection());
 		$Frame = ClassRegistry::init('Frames.Frame');
 		$Block = ClassRegistry::init('Blocks.Block');
+		$BlocksLanguage = ClassRegistry::init('Blocks.BlocksLanguage');
 		$Nc2ToNc3Frame = ClassRegistry::init('Nc2ToNc3.Nc2ToNc3Frame');
 		foreach ($nc2PhotoalbumBlocks as $nc2PhotoalbumBlock) {
 			$PhotoAlbum->begin();
